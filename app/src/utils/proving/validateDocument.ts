7ad82d5817a308d131456f2d5a6fa43f7af4fa33
Justin Hernandez <transphorm@gmail.com>
2025-07-28 12:19:51 -0700

    fix type. more opportunities

--- app/src/utils/proving/validateDocument.ts
+++ app/src/utils/proving/validateDocument.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
 import { LeanIMT } from '@openpassport/zk-kit-lean-imt';
+import type { PassportData } from '@selfxyz/common';
 import {
   API_URL,
   API_URL_STAGING,
@@ -14,7 +15,6 @@ import {
   ID_CARD_ATTESTATION_ID,
   parseCertificateSimple,
   PASSPORT_ATTESTATION_ID,
-  PassportData,
 } from '@selfxyz/common';
 import { poseidon2, poseidon5 } from 'poseidon-lite';
 
