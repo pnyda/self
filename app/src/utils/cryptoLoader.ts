d55eec8f4409a9fb6c17680e46e42b4204302d06
Justin Hernandez <transphorm@gmail.com>
2025-07-28 12:18:33 -0700

    add typing to crypto loader

--- app/src/utils/cryptoLoader.ts
+++ app/src/utils/cryptoLoader.ts
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
-export const loadCryptoUtils = async () => {
+interface CryptoUtils {
+  elliptic: typeof import('elliptic');
+  forge: typeof import('node-forge');
+  ethers: typeof import('ethers');
+}
+
+export const loadCryptoUtils = async (): Promise<CryptoUtils> => {
   const [elliptic, forge, ethers] = await Promise.all([
     import('elliptic'),
     import('node-forge'),
